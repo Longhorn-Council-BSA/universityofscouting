e6c1bed12855cd68d63cb6b6a070bded4e6fac68
Cameron King <cking@ckxng.com>
2020-06-08 09:30:06 -0500

    chore: connect to mongodb

--- README.md
+++ README.md
@@ -1,6 +1,14 @@
 # University of Scouting
 A transcript and schedule reporting tool.
 
+# Setup
+
+Create a `.env` file containing a DATABASE_URL environment variable.  This is
+a MongoDB connection string, such as you might get from mongodb.com's MongoDB
+hosting service.
+
+    DATABASE_URL=mongodb+srv://<username>:<password>@<host>/<dbname>?retryWrites=true&w=majority
+
 # Author
 - Cameron King <cameronking.me>
 - See commmit log for additional contributors.
--- app.js
+++ app.js
@@ -1,3 +1,14 @@
+// pull in environment variables from .env
+// DATABASE_URL - mongodb connection string
+require('dotenv').config();
+
+const mongoose = require('mongoose')
+
+mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true })
+const db = mongoose.connection
+db.on('error', (error) => console.error(error))
+db.once('open', () => console.log('connected to database'))
+
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
