04357a08deba612b0943f6f38bcf7c7476a9fc20
Cameron King <cking@ckxng.com>
2020-06-08 15:19:43 -0500

    fix: redirect to / from /login when logged in

--- routes/login.js
+++ routes/login.js
@@ -1,14 +1,19 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 var passport = require("passport");
 
 /* GET login page. */
-router.get('/', function(req, res, next) {
-  res.render('login', {title: "Login",user:req.user});
+router.get("/", function (req, res, next) {
+  if (req.user) {
+    res.redirect("/");
+  }
+  res.render("login", { title: "Login", user: req.user });
 });
 
 /* POST login credentials */
-router.post('/', passport.authenticate("local", {
+router.post(
+  "/",
+  passport.authenticate("local", {
     successRedirect: "/",
     failureRedirect: "/login",
     failureFlash: true,
