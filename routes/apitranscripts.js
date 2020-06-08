39ebc372da91a5858d824de4f22037773a3c5bdc
Cameron King <cking@ckxng.com>
2020-06-08 11:21:39 -0500

    feat: export transcripts with as a csv

--- routes/apitranscripts.js
+++ routes/apitranscripts.js
@@ -2,11 +2,36 @@ var express = require('express');
 var router = express.Router();
 const Transcripts = require('../models/transcriptentries')
 
-/* GET all transcript entries. */
+/**
+ * Retrieve all transcript entries from MongoDB.
+ */
+async function getAllTranscripts() {
+    const transcript = await TranscriptEntries.find();
+    response = transcript.map( (entry) => {
+        return {
+            _id: entry._id.toString(),
+            memberID: entry.memberID.toString(),
+            courseNumber: entry.courseNumber,
+            courseName: entry.courseName,
+            status: entry.status
+        }
+    });
+    console.log(response);
+    return response;
+}
+
+/** 
+ * GET all transcript entries. 
+ * if ?return=csv, return a CSV of the data instead of JSON
+*/
 router.get('/', async function(req, res, next) {
     try {
-        const transcript = await TranscriptEntries.find();
-        res.json(transcript);
+        var transcript = await getAllTranscripts();
+        if(req.query['return'] == 'csv') {
+            res.csv(transcript);
+        } else {
+            res.json(await getAllTranscripts());
+        }
     } catch(err) {
         res.status(500).json({
             message: err.message
