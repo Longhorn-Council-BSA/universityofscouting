f9fb00bc9aa9fc1fa2756c50ca1d3d210b70f993
Cameron King <cking@ckxng.com>
2020-06-08 21:07:15 -0500

    fix: correct SESSION_SECRET object name

--- app.js
+++ app.js
@@ -69,7 +69,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.use(cookieParser());
-app.use(session({ secret: process.envSESSION_SECRET }));
+app.use(session({ secret: process.env.SESSION_SECRET }));
 app.use(express.static(path.join(__dirname, "public")));
 app.use(passport.initialize());
 app.use(passport.session());
