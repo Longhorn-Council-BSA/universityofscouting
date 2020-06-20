db47c4bb26c58fff1f990ad2eb2a4b14a81ba9f6
Benjamin R. Spellmann <Benspellmann@yahoo.com>
2020-06-20 21:40:26 +0200

    add schedule page and buttons

--- app.js
+++ app.js
@@ -67,6 +67,7 @@ app.use("/", require("./routes/index"));
 app.use("/login", require("./routes/login"));
 app.use("/logout", require("./routes/logout"));
 app.use("/profile", ensureLogin.ensureLoggedIn(), require("./routes/profile"));
+app.use("/schedule", ensureLogin.ensureLoggedIn(), require("./routes/schedule"));
 app.use("/transcript", ensureLogin.ensureLoggedIn(), require("./routes/transcript"));
 app.use("/api", ensureLogin.ensureLoggedIn(), require("./routes/api"));
 // Secondary/Sub pages
