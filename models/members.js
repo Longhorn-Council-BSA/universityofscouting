ee0a18ca1698c7ca78014091a41b9a068e5593fb
Cameron King <cking@ckxng.com>
2020-08-23 18:19:19 -0500

    doc: adds documentation for virtuals in members model

--- models/members.js
+++ models/members.js
@@ -30,22 +30,53 @@ MembersSchema = new mongoose.Schema({
     }
 });
 
+/**
+ * Synthesize name into "Firstname Lastname" for ease of access
+ */
 MembersSchema.virtual('fullNameFL').get(function() {
     return this.firstName + ' ' + this.lastName;
 });
 
+/**
+ * Synthesize name into "Lastname, Firstname" for ease of access
+ */
 MembersSchema.virtual('fullNameFL').get(function() {
     return this.lastName + ', ' + this.firstName;
 });
 
+/**
+ * Synthesize council into a string representing council name for ease 
+ * of access
+ */
 MembersSchema.virtual('councilName').get(function() {
     return councils[this.councilID];
 });
 
+/**
+ * Checks capabilities of a user.
+ * 
+ * All users have an access property, which is a Number.  When a string
+ * is passed in to cap, that string will be compared against the 
+ * data/capabilities object to translate that string into a number.  The 
+ * number of the user access and the number of the capabilities are compared.
+ * If the user's access meets or exceeds the cooresponding capability number,
+ * true is returned.  Else, false is returned. 
+ * 
+ * @param {String} cap capability name
+ * @returns  true if the user's access meets the capability level, else false
+ */
 MembersSchema.methods.checkCapabilities = function(cap) {
-    return this.access >= capabilities[cap];
+    if(cap in capabilities) {
+        return this.access >= capabilities[cap];
+    }
+    return false;    
 };
 
+/**
+ * adds index for memberID+councilID
+ * 
+ * Records are typically looked up by memberID and councilID.
+ */
 MembersSchema.index({ memberID: 1, councilID: 1 });
 
 module.exports = mongoose.model('Members', MembersSchema);
