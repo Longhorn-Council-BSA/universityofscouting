01216da96847887e0db98d575b1ccb7ddcdd2af6
Cameron King <cking@ckxng.com>
2020-08-23 18:27:59 -0500

    refactor: move capabilities check to lib

--- models/members.js
+++ models/members.js
@@ -1,7 +1,6 @@
 // jshint.unstable bigint: true
 var mongoose = require('mongoose');
 require('mongoose-long')(mongoose);
-var councils = require('../data/capabilities');
 var councils = require('../data/councils');
 var SchemaTypes = mongoose.Schema.Types;
 
@@ -51,26 +50,6 @@ MembersSchema.virtual('fullNameFL').get(function() {
 MembersSchema.virtual('councilName').get(function() {
     return councils[this.councilID];
 });
-
-/**
- * Checks capabilities of a user.
- * 
- * All users have an access property, which is a Number.  When a string
- * is passed in to cap, that string will be compared against the 
- * data/capabilities object to translate that string into a number.  The 
- * number of the user access and the number of the capabilities are compared.
- * If the user's access meets or exceeds the cooresponding capability number,
- * true is returned.  Else, false is returned. 
- * 
- * @param {String} cap capability name
- * @returns  true if the user's access meets the capability level, else false
- */
-MembersSchema.methods.checkCapabilities = function(cap) {
-    if(cap in capabilities) {
-        return this.access >= capabilities[cap];
-    }
-    return false;    
-};
 
 /**
  * adds index for memberID+councilID
