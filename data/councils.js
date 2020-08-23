79ac29b216ebcd795789d16e0a53e84d222271d3
Cameron King <cking@ckxng.com>
2020-08-23 17:10:33 -0500

    feat: adds data/council module

---
+++ data/councils.js
@@ -0,0 +1,12 @@
+/**
+ * static council data module
+ *
+ * This module contains a static mapping of BSA councilID and council name.
+ *
+ * @module data/councils
+ */
+module.exports = {
+    1: 'Greater Alabama Council',
+    571: 'Circle Ten Council',
+    662: 'Longhorn Council'
+};
