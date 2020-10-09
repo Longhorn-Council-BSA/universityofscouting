91391f4fdfc68ec41dfcd64e28526a0fdcfce53e
Cameron King <cking@ckxng.com>
2020-10-08 23:01:02 -0500

    feat: filters schedule by start date

--- config/settings.js
+++ config/settings.js
@@ -7,5 +7,10 @@ module.exports = {
     /**
      * set the timezone for date math and display
      */
-    timezone: 'US/Central'
+    timezone: 'US/Central',
+
+    /**
+     * set the earliest date that will display in the schedule view
+     */
+    earliest: '2020-09-01'
 };
